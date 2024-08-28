f6b08f30c934b7358d87883619047c0fe15587e9
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-08-28 11:48:52 +0530

    setup init2

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 export const Button = ({ name, action }: ButtonProps) => {
   return (
     <button
-      className={`bg-primary text-white text-sm  font-bold py-2 px-4 rounded w-fit transition duration-150 ease-out hover:ease-in hover:scale-[0.9] `}
+      className={`bg-primary text-white text-sm font-bold py-2 px-4 rounded w-fit hover:bg-primaryDark`}
       onClick={action}
     >
       {name}
