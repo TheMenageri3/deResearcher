5a25567ce81aaa1b8323e11357ffee602f2deb0d
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-09-03 10:15:16 +0530

    feat: profile modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CreateProfile } from "@/components/CreateProfile/CreateProfile";
 import H1 from "@/components/H1";
 import P from "@/components/P";
 import { Button } from "@/components/ui/button";
@@ -9,18 +10,21 @@ export default function LandingPage() {
   return (
     <div className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <H1>deResearcher</H1>
-      <P className="text-[20px]">
+      <P className="text-[20px] font-bold">
         deResearcher - a decentralized research platform on solana
       </P>
-      <Button
-        className="text-white text-center"
-        onClick={() => {
-          console.log("clicked");
-        }}
-        size="lg"
-      >
-        Get Started
-      </Button>
+      <div className="flex flex-row justify-between w-full">
+        <Button
+          className="text-white text-center"
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          Get Started
+        </Button>
+
+        <CreateProfile />
+      </div>
     </div>
   );
 }
