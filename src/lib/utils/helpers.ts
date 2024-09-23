34ffa1515f0ed579659babb88f61491deb084c0a
Jenny <jennyliu07@gmail.com>
2024-09-24 03:38:25 +0800

    fix: build error caused by react-pdf

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const minimizePubkey = (pubkey: string) => {
   return pubkey.slice(0, 4) + "..." + pubkey.slice(-4);
 };
